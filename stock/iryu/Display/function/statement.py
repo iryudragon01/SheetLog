77920c1f74a0e66010f78e0bee963a88e320cd7d
BANDAHN NAMBOONMA <iryudragon01@gmail.com>
2019-10-30 19:51:01 +0700

    display income and expense as Table

--- stock/iryu/Display/function/statement.py
+++ stock/iryu/Display/function/statement.py
@@ -3,7 +3,6 @@ from stock.models import Income,Expense
 def getstatement(start_date):
 
     if Income.objects.filter(date_log__gt=start_date).count()>0:
-        print('hello from inside')
         income = Income.objects.filter(date_log__gt=start_date)
         expense = Expense.objects.filter(date_log__gt=start_date)
         return {'incomes': income,'expenses': expense}
